3f9408658d16ccb567742f5d418d6591fea26351
denis0001-dev <demorozov2048@gmail.com>
2024-10-16 23:07:16 +0300

    Version 1.1.1 - Improved fake crash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
