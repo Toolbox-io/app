4319ec8924e8912de403d94c7cac25d61eaa1553
denis0001-dev <demorozov2048@gmail.com>
2024-10-24 22:22:23 +0300

    1.4 - Delete protection (device admin), simplified the "permission required" dialog, redirecting you back to the app after granting the permission, option to delete the app.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 8
-        versionName = "1.3.3"
+        versionName = "1.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
