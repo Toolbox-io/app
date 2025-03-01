9c217206801846b447386a56c23693b0303632d0
denis0001-dev <demorozov2048@gmail.com>
2025-03-01 14:18:03 +0300

    v2.0

--- app/src/main/java/ru/morozovit/ultimatesecurity/ui/CoreComponents.kt
+++ app/src/main/java/ru/morozovit/ultimatesecurity/ui/CoreComponents.kt
@@ -5,15 +5,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import ru.morozovit.android.ui.Mipmap
 import ru.morozovit.android.plus
+import ru.morozovit.android.ui.Mipmap
 import ru.morozovit.ultimatesecurity.R
 
 @Suppress("NOTHING_TO_INLINE")
 @Composable
 inline fun AppIcon(modifier: Modifier = Modifier) {
     Mipmap(
-        id = R.mipmap.app_icon_3_round,
+        id = R.mipmap.app_icon,
         contentDescription = stringResource(R.string.app_name),
         modifier = Modifier.size(80.dp) + modifier
     )
