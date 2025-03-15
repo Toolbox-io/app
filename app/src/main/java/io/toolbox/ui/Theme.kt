3668bfecf0c92690663ea5f77f5607a9450ac5c5
denis0001-dev <denis0001.dev@ya.ru>
2025-03-15 16:23:36 +0300

    rename themes & optimization

--- app/src/main/java/io/toolbox/ui/Theme.kt
+++ app/src/main/java/io/toolbox/ui/Theme.kt
@@ -130,11 +130,6 @@ var theme by mutableStateOf(
 
 interface WindowInsetsScope {
     val safeDrawingInsets: WindowInsets
-    val isWindowInsetsConsumed get() =
-        isTopInsetConsumed &&
-        isBottomInsetConsumed &&
-        isLeftInsetConsumed &&
-        isRightInsetConsumed
 
     val isTopInsetConsumed: Boolean
     val isBottomInsetConsumed: Boolean
@@ -163,160 +158,152 @@ fun AppTheme(
     enforceNavContrast: Boolean = false,
     content: @Composable WindowInsetsScope.() -> Unit
 ) {
-    var colorScheme = when {
-        Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && dynamicThemeEnabled -> {
-            val context = LocalContext()
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    with (LocalContext() as Activity) {
+        var colorScheme = when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && dynamicThemeEnabled ->
+                if (darkTheme)
+                    dynamicDarkColorScheme(this)
+                else
+                    dynamicLightColorScheme(this)
+            darkTheme -> appDarkColorScheme
+            else -> appLightColorScheme
         }
-        darkTheme -> appDarkColorScheme
-        else -> appLightColorScheme
-    }
 
-    val primary by animateColorAsState(colorScheme.primary)
-    val onPrimary by animateColorAsState(colorScheme.onPrimary)
-    val primaryContainer by animateColorAsState(colorScheme.primaryContainer)
-    val onPrimaryContainer by animateColorAsState(colorScheme.onPrimaryContainer)
-    val secondary by animateColorAsState(colorScheme.secondary)
-    val onSecondary by animateColorAsState(colorScheme.onSecondary)
-    val secondaryContainer by animateColorAsState(colorScheme.secondaryContainer)
-    val onSecondaryContainer by animateColorAsState(colorScheme.onSecondaryContainer)
-    val tertiary by animateColorAsState(colorScheme.tertiary)
-    val onTertiary by animateColorAsState(colorScheme.onTertiary)
-    val tertiaryContainer by animateColorAsState(colorScheme.tertiaryContainer)
-    val onTertiaryContainer by animateColorAsState(colorScheme.onTertiaryContainer)
-    val error by animateColorAsState(colorScheme.error)
-    val onError by animateColorAsState(colorScheme.onError)
-    val errorContainer by animateColorAsState(colorScheme.errorContainer)
-    val onErrorContainer by animateColorAsState(colorScheme.onErrorContainer)
-    val background by animateColorAsState(colorScheme.background)
-    val onBackground by animateColorAsState(colorScheme.onBackground)
-    val surface by animateColorAsState(colorScheme.surface)
-    val onSurface by animateColorAsState(colorScheme.onSurface)
-    val surfaceVariant by animateColorAsState(colorScheme.surfaceVariant)
-    val onSurfaceVariant by animateColorAsState(colorScheme.onSurfaceVariant)
-    val outline by animateColorAsState(colorScheme.outline)
-    val outlineVariant by animateColorAsState(colorScheme.outlineVariant)
-    val scrim by animateColorAsState(colorScheme.scrim)
-    val inverseSurface by animateColorAsState(colorScheme.inverseSurface)
-    val inverseOnSurface by animateColorAsState(colorScheme.inverseOnSurface)
-    val inversePrimary by animateColorAsState(colorScheme.inversePrimary)
-    val surfaceDim by animateColorAsState(colorScheme.surfaceDim)
-    val surfaceBright by animateColorAsState(colorScheme.surfaceBright)
-    val surfaceContainerLowest by animateColorAsState(colorScheme.surfaceContainerLowest)
-    val surfaceContainerLow by animateColorAsState(colorScheme.surfaceContainerLow)
-    val surfaceContainer by animateColorAsState(colorScheme.surfaceContainer)
-    val surfaceContainerHigh by animateColorAsState(colorScheme.surfaceContainerHigh)
-    val surfaceContainerHighest by animateColorAsState(colorScheme.surfaceContainerHighest)
+        val primary by animateColorAsState(colorScheme.primary)
+        val onPrimary by animateColorAsState(colorScheme.onPrimary)
+        val primaryContainer by animateColorAsState(colorScheme.primaryContainer)
+        val onPrimaryContainer by animateColorAsState(colorScheme.onPrimaryContainer)
+        val secondary by animateColorAsState(colorScheme.secondary)
+        val onSecondary by animateColorAsState(colorScheme.onSecondary)
+        val secondaryContainer by animateColorAsState(colorScheme.secondaryContainer)
+        val onSecondaryContainer by animateColorAsState(colorScheme.onSecondaryContainer)
+        val tertiary by animateColorAsState(colorScheme.tertiary)
+        val onTertiary by animateColorAsState(colorScheme.onTertiary)
+        val tertiaryContainer by animateColorAsState(colorScheme.tertiaryContainer)
+        val onTertiaryContainer by animateColorAsState(colorScheme.onTertiaryContainer)
+        val error by animateColorAsState(colorScheme.error)
+        val onError by animateColorAsState(colorScheme.onError)
+        val errorContainer by animateColorAsState(colorScheme.errorContainer)
+        val onErrorContainer by animateColorAsState(colorScheme.onErrorContainer)
+        val background by animateColorAsState(colorScheme.background)
+        val onBackground by animateColorAsState(colorScheme.onBackground)
+        val surface by animateColorAsState(colorScheme.surface)
+        val onSurface by animateColorAsState(colorScheme.onSurface)
+        val surfaceVariant by animateColorAsState(colorScheme.surfaceVariant)
+        val onSurfaceVariant by animateColorAsState(colorScheme.onSurfaceVariant)
+        val outline by animateColorAsState(colorScheme.outline)
+        val outlineVariant by animateColorAsState(colorScheme.outlineVariant)
+        val scrim by animateColorAsState(colorScheme.scrim)
+        val inverseSurface by animateColorAsState(colorScheme.inverseSurface)
+        val inverseOnSurface by animateColorAsState(colorScheme.inverseOnSurface)
+        val inversePrimary by animateColorAsState(colorScheme.inversePrimary)
+        val surfaceDim by animateColorAsState(colorScheme.surfaceDim)
+        val surfaceBright by animateColorAsState(colorScheme.surfaceBright)
+        val surfaceContainerLowest by animateColorAsState(colorScheme.surfaceContainerLowest)
+        val surfaceContainerLow by animateColorAsState(colorScheme.surfaceContainerLow)
+        val surfaceContainer by animateColorAsState(colorScheme.surfaceContainer)
+        val surfaceContainerHigh by animateColorAsState(colorScheme.surfaceContainerHigh)
+        val surfaceContainerHighest by animateColorAsState(colorScheme.surfaceContainerHighest)
 
-    colorScheme = colorScheme.copy(
-        primary = primary,
-        onPrimary = onPrimary,
-        primaryContainer = primaryContainer,
-        onPrimaryContainer = onPrimaryContainer,
-        secondary = secondary,
-        onSecondary = onSecondary,
-        secondaryContainer = secondaryContainer,
-        onSecondaryContainer = onSecondaryContainer,
-        tertiary = tertiary,
-        onTertiary = onTertiary,
-        tertiaryContainer = tertiaryContainer,
-        onTertiaryContainer = onTertiaryContainer,
-        error = error,
-        onError = onError,
-        errorContainer = errorContainer,
-        onErrorContainer = onErrorContainer,
-        background = background,
-        onBackground = onBackground,
-        surface = surface,
-        onSurface = onSurface,
-        surfaceVariant = surfaceVariant,
-        onSurfaceVariant = onSurfaceVariant,
-        outline = outline,
-        outlineVariant = outlineVariant,
-        scrim = scrim,
-        inverseSurface = inverseSurface,
-        inverseOnSurface = inverseOnSurface,
-        inversePrimary = inversePrimary,
-        surfaceDim = surfaceDim,
-        surfaceBright = surfaceBright,
-        surfaceContainerLowest = surfaceContainerLowest,
-        surfaceContainerLow = surfaceContainerLow,
-        surfaceContainer = surfaceContainer,
-        surfaceContainerHigh = surfaceContainerHigh,
-        surfaceContainerHighest = surfaceContainerHighest
-    )
-
-    WindowCompat.getInsetsController(
-        (LocalContext() as Activity).window,
-        LocalView()
-    ).isAppearanceLightStatusBars = !darkTheme
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        (LocalContext() as Activity).window.isNavigationBarContrastEnforced = enforceNavContrast
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-    ) {
-        val insets = WindowInsets(
-            WindowInsets.safeDrawing.getLeft(
-                LocalDensity(),
-                LocalLayoutDirection()
-            ),
-            WindowInsets.safeDrawing.getTop(LocalDensity()),
-            WindowInsets.safeDrawing.getRight(
-                LocalDensity(),
-                LocalLayoutDirection()
-            ),
-            WindowInsets.safeDrawing.getBottom(LocalDensity())
+        colorScheme = colorScheme.copy(
+            primary = primary,
+            onPrimary = onPrimary,
+            primaryContainer = primaryContainer,
+            onPrimaryContainer = onPrimaryContainer,
+            secondary = secondary,
+            onSecondary = onSecondary,
+            secondaryContainer = secondaryContainer,
+            onSecondaryContainer = onSecondaryContainer,
+            tertiary = tertiary,
+            onTertiary = onTertiary,
+            tertiaryContainer = tertiaryContainer,
+            onTertiaryContainer = onTertiaryContainer,
+            error = error,
+            onError = onError,
+            errorContainer = errorContainer,
+            onErrorContainer = onErrorContainer,
+            background = background,
+            onBackground = onBackground,
+            surface = surface,
+            onSurface = onSurface,
+            surfaceVariant = surfaceVariant,
+            onSurfaceVariant = onSurfaceVariant,
+            outline = outline,
+            outlineVariant = outlineVariant,
+            scrim = scrim,
+            inverseSurface = inverseSurface,
+            inverseOnSurface = inverseOnSurface,
+            inversePrimary = inversePrimary,
+            surfaceDim = surfaceDim,
+            surfaceBright = surfaceBright,
+            surfaceContainerLowest = surfaceContainerLowest,
+            surfaceContainerLow = surfaceContainerLow,
+            surfaceContainer = surfaceContainer,
+            surfaceContainerHigh = surfaceContainerHigh,
+            surfaceContainerHighest = surfaceContainerHighest
         )
 
-        WindowInsetsHandler(modifier, !consumeLeftInsets, !consumeRightInsets) {
-            Surface(
-                contentColor = colorScheme.onSurface,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .let {
-                        var mod = it
-                        if (consumeTopInsets) {
-                            mod += Modifier.consumeWindowInsets(WindowInsets.safeContent.only(WindowInsetsSides.Top))
-                        }
-                        if (consumeBottomInsets) {
-                            mod += Modifier.consumeWindowInsets(WindowInsets.safeContent.only(WindowInsetsSides.Bottom))
-                        }
-                        if (!consumeLeftInsets) {
-                            mod += Modifier.consumeWindowInsets(WindowInsets.safeContent.only(WindowInsetsSides.Left))
-                        }
-                        if (!consumeRightInsets) {
-                            mod += Modifier.consumeWindowInsets(WindowInsets.safeContent.only(WindowInsetsSides.Right))
-                        }
-                        mod
-                    }
-            ) {
-                val topInset = insets.getTop(LocalDensity())
-                val bottomInset = insets.getBottom(LocalDensity())
-                val leftInset = insets.getLeft(LocalDensity(), LocalLayoutDirection())
-                val rightInset = insets.getRight(LocalDensity(), LocalLayoutDirection())
+        WindowCompat.getInsetsController(window, LocalView()).isAppearanceLightStatusBars = !darkTheme
 
-                content(
-                    object : WindowInsetsScope {
-                        override val safeDrawingInsets: WindowInsets get() = insets
-                        override val isTopInsetConsumed: Boolean get() = consumeTopInsets
-                        override val isBottomInsetConsumed: Boolean get() = consumeBottomInsets
-                        override val isLeftInsetConsumed: Boolean get() = consumeLeftInsets
-                        override val isRightInsetConsumed: Boolean get() = consumeRightInsets
-                        override val isWindowInsetsConsumed
-                            get() =
-                                consumeTopInsets &&
-                                consumeBottomInsets &&
-                                consumeLeftInsets &&
-                                consumeRightInsets
-                        override val topInset: Int get() = topInset
-                        override val bottomInset: Int get() = bottomInset
-                        override val leftInset: Int get() = leftInset
-                        override val rightInset: Int get() = rightInset
-                    }
-                )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.isNavigationBarContrastEnforced = enforceNavContrast
+        }
+
+        MaterialTheme(colorScheme = colorScheme) {
+            val insets = WindowInsets(
+                WindowInsets.safeDrawing.getLeft(
+                    LocalDensity(),
+                    LocalLayoutDirection()
+                ),
+                WindowInsets.safeDrawing.getTop(LocalDensity()),
+                WindowInsets.safeDrawing.getRight(
+                    LocalDensity(),
+                    LocalLayoutDirection()
+                ),
+                WindowInsets.safeDrawing.getBottom(LocalDensity())
+            )
+
+            WindowInsetsHandler(modifier, !consumeLeftInsets, !consumeRightInsets) {
+                Surface(
+                    contentColor = colorScheme.onSurface,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .let {
+                            var mod = it
+                            if (consumeTopInsets) {
+                                mod += Modifier.consumeWindowInsets(WindowInsets.safeContent.only(WindowInsetsSides.Top))
+                            }
+                            if (consumeBottomInsets) {
+                                mod += Modifier.consumeWindowInsets(WindowInsets.safeContent.only(WindowInsetsSides.Bottom))
+                            }
+                            if (!consumeLeftInsets) {
+                                mod += Modifier.consumeWindowInsets(WindowInsets.safeContent.only(WindowInsetsSides.Left))
+                            }
+                            if (!consumeRightInsets) {
+                                mod += Modifier.consumeWindowInsets(WindowInsets.safeContent.only(WindowInsetsSides.Right))
+                            }
+                            mod
+                        }
+                ) {
+                    val topInset = insets.getTop(LocalDensity())
+                    val bottomInset = insets.getBottom(LocalDensity())
+                    val leftInset = insets.getLeft(LocalDensity(), LocalLayoutDirection())
+                    val rightInset = insets.getRight(LocalDensity(), LocalLayoutDirection())
+
+                    content(
+                        object : WindowInsetsScope {
+                            override val safeDrawingInsets: WindowInsets get() = insets
+                            override val isTopInsetConsumed: Boolean get() = consumeTopInsets
+                            override val isBottomInsetConsumed: Boolean get() = consumeBottomInsets
+                            override val isLeftInsetConsumed: Boolean get() = consumeLeftInsets
+                            override val isRightInsetConsumed: Boolean get() = consumeRightInsets
+                            override val topInset: Int get() = topInset
+                            override val bottomInset: Int get() = bottomInset
+                            override val leftInset: Int get() = leftInset
+                            override val rightInset: Int get() = rightInset
+                        }
+                    )
+                }
             }
         }
     }
